e8f64af17a268c6eb3b4d175b78775f4e63fe0d0
ndolocr <crndolo@gmail.com>
2024-06-16 00:49:57 +0300

    Creating Question App

--- tags/models.py
+++ tags/models.py
@@ -4,5 +4,8 @@ from django.db import models
 class Tag(models.Model):
     name = models.CharField(max_length=255, unique=True)
 
+    updated_on = models.DateTimeField(auto_now=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+
     def __str__(self):
         return self.name
