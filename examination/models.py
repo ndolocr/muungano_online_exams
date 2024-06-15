509d8a0cec7abdb541d680da5e6006166247666c
ndolocr <crndolo@gmail.com>
2024-06-15 23:32:08 +0300

    Create model for examination app

--- examination/models.py
+++ examination/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+from examination_body.models import ExaminationBody
+
 # Create your models here.
+class Examination(models.Model):
+    slug = models.SlugField(max_length=255, null = True, blank = True)
+    name = models.CharField(max_length=255, null = False, blank = False)
+    acronym = models.CharField(max_length=255, null = False, blank = False)
+    examination_body = models.ForeignKey(ExaminationBody, on_delete=models.CASCADE)
+
+    updated_on = models.DateTimeField(auto_now=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.acronym
