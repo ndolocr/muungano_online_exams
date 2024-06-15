56b74e1e2b503045dce61185850ac90f992d9ea0
ndolocr <crndolo@gmail.com>
2024-06-15 22:50:04 +0300

    Create Model for Examination Body App.

--- examination_body/models.py
+++ examination_body/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class ExaminationBody(models.Model):
+    slug = models.SlugField(max_length=255, null = True, blank = True)
+    name = models.CharField(max_length=255, null = False, blank = False)
+    acronym = models.CharField(max_length=255, null = False, blank = False)
+    
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.acronym
+
