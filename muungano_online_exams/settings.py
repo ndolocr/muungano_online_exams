21a1935d922ff3dc5e4b9270c7bb73192ef42219
ndolocr <crndolo@gmail.com>
2024-06-18 22:36:44 +0300

    Include static files settings

--- muungano_online_exams/settings.py
+++ muungano_online_exams/settings.py
@@ -129,6 +129,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / 'static',]
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+    },
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
