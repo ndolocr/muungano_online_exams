ddf4f0ce4cb78c59f201976eb7ac28b1f5c00e1c
ndolocr <crndolo@gmail.com>
2024-06-23 15:08:53 +0300

    Create model for table app

--- table/models.py
+++ table/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Table(models.Model):
+    table_name = models.CharField(max_length=255, null = False, blank = False)
+    no_of_rows = models.CharField(max_length=255, null = False, blank = False)
+    no_of_columns = models.CharField(max_length=255, null = False, blank = False)
+    
+    updated_on = models.DateTimeField(auto_now=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.question_number
