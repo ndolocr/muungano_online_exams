cd2ec2eb27352d308de751324892e2c2b93b291d
ndolocr <crndolo@gmail.com>
2024-08-04 14:06:36 +0300

    Include Examination urls into main urls

--- examination/urls.py
+++ examination/urls.py
@@ -6,4 +6,5 @@ app_name = "examination_app"
 
 urlpatterns = [
     path('create', views.create_examination, name='create_examination'),
+    path('view/all', views.view_all_examinations, name='view_all_examinations'),
 ]
--- muungano_online_exams/urls.py
+++ muungano_online_exams/urls.py
@@ -20,5 +20,6 @@ from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('examination/', include('examination.urls')),
     path('examination/body/', include('examination_body.urls')),
 ]
