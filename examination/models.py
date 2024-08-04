6e595c43641052b9e46b763778fc9d924ddd7c95
ndolocr <crndolo@gmail.com>
2024-08-04 13:07:46 +0300

    Create template to view and create examination body

--- examination/models.py
+++ examination/models.py
@@ -5,8 +5,8 @@ from examination_body.models import ExaminationBody
 # Create your models here.
 class Examination(models.Model):
     slug = models.SlugField(max_length=255, null = True, blank = True)
-    name = models.CharField(max_length=255, null = False, blank = False)
-    acronym = models.CharField(max_length=255, null = False, blank = False)
+    name = models.CharField(max_length=255, null = False, blank = False) # Kenya Certificate of Secondary Education
+    acronym = models.CharField(max_length=255, null = False, blank = False) # KCSE
     
     examination_body = models.ForeignKey(ExaminationBody, on_delete=models.CASCADE)
 
