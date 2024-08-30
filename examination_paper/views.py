178d2ccbde28a21eeff539b92adf31145fd3b518
Raphael Ndolo <raphaelndolo@Raphaels-MacBook-Pro.local>
2024-08-30 08:37:14 +0300

    Initiate form to create examination body

--- examination_paper/views.py
+++ examination_paper/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 
+from examination_body.views import ExaminationBody
 # Create your views here.
 
 def create(request):
     if request.method == "POST":
         pass
     else:
-        return render(request, 'examination_paper/create.html')
+        examination_bodies = ExaminationBody.objects.all()
+
+        context = {
+            "examination_bodies": examination_bodies,
+        }
+        return render(request, 'examination_paper/create.html', context)
