b34dab359fdd4b78645f2ef6b43fbe851d5b8cec
ndolocr <crndolo@gmail.com>
2024-08-31 10:15:34 +0300

    Database Settings.

--- muungano_online_exams/settings.py
+++ muungano_online_exams/settings.py
@@ -91,8 +91,12 @@ WSGI_APPLICATION = 'muungano_online_exams.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'mysql.connector.django',
+        'NAME': 'worldcheck',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST':'127.0.0.1',
+        'PORT':'8889',
     }
 }
 
