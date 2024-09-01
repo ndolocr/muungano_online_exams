003203d8d21fed7d50b3c4a3ca40679893016270
ndolocr <crndolo@gmail.com>
2024-09-01 16:45:26 +0300

    Create urls for examination paper

--- examination_paper/urls.py
+++ examination_paper/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from examination_paper import views
 
+app_name = "examination_paper_app"
+
 urlpatterns = [
-    path('create', views.create, name='create_examination_paper'),
+    path('create', views.create, name='create'),
 ]
