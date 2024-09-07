fdc99018633cf2efaecad7e0dc05131a0eb24ec1
ndolocr <crndolo@gmail.com>
2024-09-07 08:09:05 +0300

    Created a url to view all examination papers

--- examination_paper/urls.py
+++ examination_paper/urls.py
@@ -6,6 +6,7 @@ app_name = "examination_paper_app"
 
 urlpatterns = [
     path('create', views.create, name='create'),
+    path('view/all', views.view_all, name='view_all'),
     path('create/insturctions/<int:pk>', views.create_instructions, name='create_instructions'),
     path('create/insturctions/post', views.create_instructions_post, name='create_instructions_post'),
 ]
